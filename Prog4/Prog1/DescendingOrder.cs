1c83db3529717400917c25c89cc3f78112561a02
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 20:31:35 -0400

    Add files via upload

---
+++ Prog4/Prog1/DescendingOrder.cs
@@ -0,0 +1,36 @@
+﻿// Program 4
+// CIS 200-01
+// Due: 4/17/2019
+// By: L5135
+
+// File: DescendingOrder.cs
+// This file compare two items by Copyright Year in Descending order.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryItems
+{
+    public class DescendingOrder : Comparer<LibraryItem>
+    {
+        // Precondition:  None
+        // Postcondition: Reverses natural time order, so descending
+        //                When item11 < item2, method returns positive #
+        //                When item1 == item2, method returns zero
+        //                When item1 > item2, method returns negative #
+        public override int Compare(LibraryItem item1, LibraryItem item2)
+        {
+            // Ensure correct handling of null values (in .NET, null less than anything)
+            if (item1 == null && item2 == null) //Both null?
+                return 0; //Equal
+            if (item1 == null) //only item1 null?
+                return -1; //null is less than actual item
+            if (item2 == null)//only item2 null?
+                return 1;//actual item is greater than null
+            return (-1) * item1.CopyrightYear.CompareTo(item2.CopyrightYear);// Reverses natural order, so descending
+
+
+        } 
+    }
+}
---
+++ Prog4/Prog4/Prog1/Sort.cs
@@ -0,0 +1,34 @@
+﻿// Program 4
+// CIS 200-01
+// Due: 4/17/2019
+// By: L5135
+
+// File: Sort.cs
+// This file compare two items by Title and Type in Ascending order.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryItems
+{
+    public class Sort: Comparer<LibraryItem>
+    {
+        // Precondition:  None
+        // Postcondition: Ascending Order
+        //                When item11 == null, method returns 1
+        //                When item1 == item2, method returns the compared value
+        public override int Compare(LibraryItem item1, LibraryItem item2)
+        {
+            if (item1 == null) // only item1 is null?
+                return 1;   // equals 1
+
+
+            if (item1.GetType() == (item2.GetType()))//Item1 and Item2 are equal
+                return item1.CompareTo(item2);       //Then compare them 
+            else
+                return (item1.GetType().ToString().CompareTo(item2.GetType().ToString())); // compare the types of item1 to item2 by making them strings
+
+        }
+    }
+}
