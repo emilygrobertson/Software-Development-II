b60a9928859a358c98fd432b26a64093588a0266
emilygrobertson <53453748+emilygrobertson@users.noreply.github.com>
2019-07-29 20:32:19 -0400

    Add files via upload

---
+++ Program5/ConsoleApp3/BinaryTreeLibrary2.cs
@@ -0,0 +1,117 @@
+﻿// Grading ID: L5135
+//Program 5-EC
+//Due Date: 4/26/2019
+//CIS 200-01
+// Declaration of class TreeNode and class Tree.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+   // class TreeNode declaration to make this class generic and comprable 
+    public class TreeNode<T> where T : IComparable<T>
+    {
+      // automatic property LeftNode
+      //Precondition: This class must be generic
+      //Post Condition: This will be autoimplemented
+      public TreeNode<T> LeftNode { get; set; }
+
+        // automatic property Data
+       //Precondition: This class must be generic
+       //Post Condition: This will be autoimplemented
+        public IComparable Data { get; private set; }
+
+        // automatic property RightNode
+        //Precondition: This class must be generic
+        //Post Condition: This will be autoimplemented
+        public TreeNode<T> RightNode { get; set; }
+
+      // initialize Data and make this a leaf node
+      public TreeNode(IComparable nodeData)
+      {
+         Data = nodeData;
+      }
+
+      // insert TreeNode into Tree that contains nodes;
+      // ignore duplicate values
+      public void Insert(IComparable insertValue)
+      {
+         if (insertValue.CompareTo(Data) < 0) // insert in left subtree
+         {
+            // insert new TreeNode
+            if (LeftNode == null)
+            {
+               LeftNode = new TreeNode<T>(insertValue);
+            }
+            else // continue traversing left subtree
+            {
+               LeftNode.Insert(insertValue);
+            }
+         }
+         else if (insertValue.CompareTo(Data) > 0) // insert in right
+         {
+            // insert new TreeNode
+            if (RightNode == null)
+            {
+               RightNode = new TreeNode<T>(insertValue);
+            }
+            else // continue traversing right subtree
+            {
+               RightNode.Insert(insertValue);
+            }
+         }
+      }
+   }
+
+   // class Tree declaration
+   public class Tree<T> where T : IComparable<T>
+    {
+      private TreeNode<T> root;
+
+      // Insert a new node in the binary search tree.
+      // If the root node is null, create the root node here.
+      // Otherwise, call the insert method of class TreeNode.
+      public void InsertNode(IComparable insertValue)
+      {
+         if (root == null)
+         {
+            root = new TreeNode<T>(insertValue);
+         }
+         else
+         {
+            root.Insert(insertValue);
+         }
+      }
+
+      // begin inorder traversal
+      public void InorderTraversal()
+      {
+         InorderHelper(root);
+      }
+
+      // recursive method to perform inorder traversal
+      private void InorderHelper(TreeNode<T> node)
+      {
+         if (node != null)
+         {
+            // traverse left subtree
+            InorderHelper(node.LeftNode);
+
+            // output node data
+            Console.Write($"{node.Data} ");
+
+            // traverse right subtree
+            InorderHelper(node.RightNode);
+         }
+      }
+
+     
+
+     
+   }
+}
+
+
---
+++ Program5/ConsoleApp3/Program.cs
@@ -0,0 +1,81 @@
+﻿// Grading ID: L5135
+//Program 5-EC
+//Due Date: 4/26/2019
+//CIS 200-01
+// Test Data is in this class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Test data
+            int[] intArray = { 2, 4, 7, 8, 3, 9, 0, 1, 5, 6 };
+            double[] doubleArray = { 8.8, 2.2, 4.4, 3.3, 1.1, 7.7, 5.5, 6.6, 9.9, 1.1 };
+            string[] stringArray =
+               {"eight", "two", "four","three", "one", "seven", "five", "six", "nine","ten"};
+
+            // create int Tree
+            Tree<int> intTree = new Tree<int>();
+            Console.WriteLine($"\n\n\nInserting into {nameof(intTree)}:");
+           
+            foreach (IComparable data in intArray)
+            {
+                Console.Write($"{data} ");
+                intTree.InsertNode(data);
+            }
+            
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {nameof(intTree)}");
+            intTree.InorderTraversal();
+            Pause();
+
+            // create double Tree
+            Tree<double> doubleTree = new Tree<double>();
+            Console.WriteLine($"\n\n\nInserting into {nameof(doubleTree)}:");
+
+            foreach (IComparable data in doubleArray)
+            {
+                Console.Write($"{data} ");
+                doubleTree.InsertNode(data);
+            }
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {nameof(doubleTree)}");
+            doubleTree.InorderTraversal();
+            Pause();
+
+            // create string Tree
+            Tree<string> stringTree = new Tree<string>();
+            Console.WriteLine($"\n\n\nInserting into {nameof(stringTree)}:");
+
+            foreach (IComparable data in stringArray)
+            {
+                Console.Write($"{data} ");
+                stringTree.InsertNode(data);
+            }
+            // perform inorder traversal of tree
+            Console.WriteLine($"\n\nInorder traversal of {nameof(stringTree)}");
+            stringTree.InorderTraversal();
+            Pause();
+
+            
+        }
+        // Precondition:  None
+        // Postcondition: Pauses program execution until user presses Enter and
+        //                then clears the screen
+        public static void Pause()
+        {
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.Clear();  //Clears the screen
+        }
+    }
+    
+}
+
